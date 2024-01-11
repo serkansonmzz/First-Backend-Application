925e27637a649d14be6652f193c4f18607e09364
Serkan Sönmez <serkansonmzz@gmail.com>
2024-01-11 11:42:34 +0300

    router and express configuration was integrated. port 8080 was created and run in index.js

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import router from "./expressRouter.js";
+const PORT = 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use("/", router);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
